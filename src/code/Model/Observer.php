3454183a2c8d1e5c3bf1efb5b709b53cb1d8b52f
Mathis Klooß <mathis@mage-profis.de>
2017-04-03 13:35:18 +0200

    fixed okay a bit more digs

--- src/code/Model/Observer.php
+++ src/code/Model/Observer.php
@@ -18,11 +18,11 @@ extends Mage_Core_Model_Abstract
         {
             $customer = Mage::getModel('customer/customer')->load((int) $_id);
             /* @var $customer Mage_Customer_Model_Customer */
-            if ($customer->getId() && strlen((string) $customer->getData('confirmation')) > 0)
+            if ($customer->getId() && strlen((string) $customer->getData('confirmation')) > 3)
             {
                 try {
                     $customer->delete();
-                    Mage::log('Customer: "'. $customer->getEmail(). '" has been deleted.');
+                    Mage::log('Customer: "'. $customer->getEmail(). '" has been deleted.', null, 'customer_removed.log');
                 } catch (Exception $e) {
                     Mage::logException($e);
                 }
